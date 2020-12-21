2b0d0d4e032b1b4fc99e6990f989406c06fe3c4d
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2020-12-21 12:56:32 +0100

    Trójkąt działą

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,148 @@
 #include <iostream>
 
-#include <SDL2/SDL.h>
 #include <GL/glew.h>
-#include <SDL2/SDL_opengl.h>
+#include <GLFW/glfw3.h>
 
-const GLint WIDTH = 800, HEIGHT = 600;
+#include <string>
+#include <iostream>
+#include <alloca.h>
 
-int main(int argc, char *argv[])
-{
-    SDL_Init(SDL_INIT_EVERYTHING);
+const GLint WIDTH = 1280, HEIGHT = 720;
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+static unsigned compile_shader(unsigned type, std::string& src) {
+    auto id = glCreateShader(type);
+    auto src_c_str = src.c_str();
+    glShaderSource(id, 1, &src_c_str, nullptr);
+    glCompileShader(id);
 
-    SDL_Window *window = SDL_CreateWindow("OpenGL", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_OPENGL);
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE) {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        auto msg = (char*) alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, msg);
+        std::cout << "Failed to compile "
+            << (type == GL_VERTEX_SHADER ? "vertex" : "fragment")
+            << " shader:\n" << msg << std::endl;
+        return 0;
+    }
 
-    SDL_GLContext context = SDL_GL_CreateContext(window);
+    return id;
+}
+
+static unsigned create_shader(std::string& vert_shader, std::string& frag_shader) {
+    auto program = glCreateProgram();
+    auto vs = compile_shader(GL_VERTEX_SHADER, vert_shader);
+    auto fs = compile_shader(GL_FRAGMENT_SHADER, frag_shader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return program;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    auto window = glfwCreateWindow(WIDTH, HEIGHT, "Silnik 3D", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
 
     glewExperimental = GL_TRUE;
-    if (GLEW_OK != glewInit())
-    {
+    if (GLEW_OK != glewInit()) {
         std::cout << "Failed to initialize GLEW" << std::endl;
         return EXIT_FAILURE;
     }
 
     glViewport(0, 0, WIDTH, HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    SDL_Event windowEvent;
-    bool should_exit = false;
+    // SETUP END
 
-    while (!should_exit) {
-        while (SDL_PollEvent(&windowEvent) != 0) {
-            if (windowEvent.type == SDL_QUIT) should_exit = true;
-        }
+    float positions[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f
+    };
 
+    unsigned VBO;
+    unsigned VAO;
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), 0);
+    glEnableVertexAttribArray(0);
+
+    std::string vertex_shader =
+        "#version 330 core\n"
+        "layout(location = 0) in vec3 position;\n"
+        "\n"
+        "void main() {\n"
+        "   gl_Position = vec4(position.x, position.y, position.z, 1.0f);\n"
+        "}\n";
+
+    std::string fragment_shader =
+        "#version 330 core\n"
+        "out vec4 color;\n"
+        "\n"
+        "void main() {\n"
+        "   color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n";
+
+    auto program = create_shader(vertex_shader, fragment_shader);
+    glUseProgram(program);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        process_input(window);
+
+        
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        SDL_GL_SwapWindow(window);
+        glUseProgram(program);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        auto error = glGetError();
+        if (error != GL_NO_ERROR) {
+            std::cout << "error: " << gluErrorString(error) << std::endl;
+        }
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
 
-    SDL_GL_DeleteContext(context);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
